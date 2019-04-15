444ba9aedd786eb9089c9bd8244feb45045f080b
jsxgod <Smykoo7@gmail.com>
2019-04-15 02:50:20 +0200

    Initial commit

---
+++ src/main/java/js/springexamples/catapp/CatAppApplication.java
@@ -0,0 +1,13 @@
+package js.springexamples.catapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatAppApplication.class, args);
+	}
+
+}
